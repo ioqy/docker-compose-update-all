c417f95e1b91d808bbe675499646377b2bce1de9
ioqy <95605581+ioqy@users.noreply.github.com>
2022-11-26 14:27:12 +0100

    Added:
    * pulling of latest images
    * skipping of ignored project directories
    * prune old images

--- docker-compose-update-all.sh
+++ docker-compose-update-all.sh
@@ -3,6 +3,13 @@
 # lock script to prevent parallel execution
 [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
 
+export LOG_LEVEL=error
+
+# pull all images
+for image in $(docker images --format "{{.Repository}}:{{.Tag}}" | grep -v '<none>');do
+  docker pull $image --quiet
+done
+
 # iterate through running docker compose files
 docker compose ls | tail -n+2 | while read name status configfile; do
 
@@ -11,8 +18,13 @@ docker compose ls | tail -n+2 | while read name status configfile; do
 
   projectdirectory="$(dirname $configfile)"
 
+  # skip ignored project directories
+  [ -f "$projectdirectory/.dockerupdateignore" ] && continue
+
   # pull images and restart containers
-  docker compose --project-directory "$projectdirectory" pull \
-    && docker compose --project-directory "$projectdirectory" up --detach --remove-orphans
+  docker compose --project-directory "$projectdirectory" pull --quiet \
+    && docker compose --project-directory "$projectdirectory" up --detach --remove-orphans --quiet-pull
 
 done
+
+docker image prune --force
